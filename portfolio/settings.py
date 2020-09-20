e8f0d2a2b656aa985a083263d4a5356414212e91
AVIJIT <avijit.batabyal@gmail.com>
2020-09-20 19:34:12 +0530

    My portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,8 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'portfolio/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-MEDIA_URL = '/coolstuff/'
+MEDIA_URL = '/media/'
